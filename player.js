2e8186860b75ebcfbb2b1ac98d1a6a32058fe76e
Freshmaker9 <trent_mccarthy_9@hotmail.com>
2015-05-31 15:41:32 +1000

    bullets working
    everything working good

--- player.js
+++ player.js
@@ -5,13 +5,13 @@ var RIGHT = 1;
 var ANIM_IDLE_LEFT = 0;
 var ANIM_JUMP_LEFT = 1;
 var ANIM_WALK_LEFT = 2;
-//var ANIM_SHOOT_LEFT = 3;
+var ANIM_SHOOT_LEFT = 3;
 //var ANIM_CLIMB = 4;
-var ANIM_IDLE_RIGHT = 3;
-var ANIM_JUMP_RIGHT = 4;
-var ANIM_WALK_RIGHT = 5;
-//cvar ANIM_SHOOT_RIGHT = 8;
-var ANIM_MAX = 6;
+var ANIM_IDLE_RIGHT = 4;
+var ANIM_JUMP_RIGHT = 5;
+var ANIM_WALK_RIGHT = 6;
+var ANIM_SHOOT_RIGHT = 7;
+var ANIM_MAX = 8;
 
 var PLAYER_SPEED = 300;
 
@@ -32,13 +32,19 @@ this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
 [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);    // walk left
 
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]);    // shoot left
+
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
 [52, 53, 54, 55, 56, 57, 58, 59]);                       // idle right
 
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05,        // jump right
 [60, 61, 62, 63, 64]);
 
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
-[65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);    // walk right
+[65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);      //walk right
+
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92]);      // shoot right
 
 
 
@@ -141,7 +147,33 @@ var ddy = GRAVITY;
 if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true && this.cooldownTimer <=0) 
 {
 	var tempBullet = new Bullet(this.position.x, this.position.y, LEFT, RIGHT, deltaTime);
+	if (this.direction == LEFT)
+	{
+		left = true;
+		if(this.sprite.currentAnimation != ANIM_SHOOT_LEFT)
+			this.sprite.setAnimation(ANIM_SHOOT_LEFT);
+	}
+
+	else 
+	{
+		right = true;
+		if (this.sprite.currentAnimation != ANIM_SHOOT_RIGHT)
+			this.sprite.setAnimation(ANIM_SHOOT_RIGHT);
+	}
+
+	if(right == true)
+	{
+		tempBullet.velocity.x = 400;
+		tempBullet.position.x += 80;
+	}
+
+	else 
+	{
+		tempBullet.velocity.x = -400;
+		tempBullet.position.x -= 50;
+	}
 	/*this.moveRight = wasright;
+
 if(this.moveRight == true)
 		{ 
 		this.velocity.set(MAXDX *2, 1);
